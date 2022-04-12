09d7a159798de2f4ead3f7071eab80c8bdb7c515
GitHub Action <action@github.com>
2022-04-12 09:11:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangruifeng99/datapackhint/MyBundle.kt
+++ src/main/kotlin/com/github/wangruifeng99/datapackhint/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wangruifeng99.datapackhint
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/wangruifeng99/datapackhint/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wangruifeng99.datapackhint.services
+
+import com.github.wangruifeng99.datapackhint.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
